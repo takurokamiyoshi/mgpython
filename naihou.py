a46d3ea24fb033731f50adf1f32af76875944741
takurokamiyoshi <tkamiyoshi@kyi.biglobe.ne.jp>
2016-02-18 00:13:28 +0900

    Update naihou.py

--- naihou.py
+++ naihou.py
@@ -5,7 +5,7 @@ list={'yasuhiro':{'nike':5,'adidas':2,'puma':4},'takuro':{'adidas':5,'nike':4,'a
 
 #リスト内包です。1文で書けるので楽です。辞書型で共通するキーを引っ張ってきます
 y=[x for x in list['yasuhiro'] if x in list['takuro']]
-  print y
+print y
 
 #共通しているキーの平均点をリスト型で出します
 z=[(x,(list['yasuhiro'][x]+list['takuro'][x])/2.0) for x in list['yasuhiro'] if x in list['takuro']]
